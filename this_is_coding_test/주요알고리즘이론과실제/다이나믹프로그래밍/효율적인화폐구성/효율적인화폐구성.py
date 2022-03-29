11a72f68b54d6f359813b4051d19935bcab40d58
chaaaning <yunjch21@gmail.com>
2022-03-29 17:29:14 +0900

    Dynamic Programming second commit

---
+++ this_is_coding_test/주요알고리즘이론과실제/다이나믹프로그래밍/효율적인화폐구성/효율적인화폐구성.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+currency = []
+
+for _ in range(n):
+    currency.append(int(input()))
+
+d = [10001]*(m+1)
+
+d[0] = 0
+
+for i in range(n):
+    for j in range(currency[i], m+1):
+        if d[j-currency[i]] != 10001:
+            d[j] = min(d[j], d[j-currency[i]]+1)
+            
+if d[m] == 10001: print(-1)
+else: print(d[m])
+    
+    
+    
